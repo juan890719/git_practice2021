8215c15747712d2a60b1de593b9232e83d712421
Juan <michelle.70623@gmail.com>
2021-03-09 16:28:44 +0800

    Student.class new a print()

--- src/main/java/com/tom/HelloWorld.java
+++ src/main/java/com/tom/HelloWorld.java
@@ -3,5 +3,16 @@ package com.tom;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //Student stu = new Student("1001","Jack", 60, 80);
+        //stu.print();
+        Student[] students = {
+                new Student("1001","Jack", 60, 80),
+                new Student("1002","Fin", 60, 80),
+                new Student("1003","Polo", 60, 80),
+                new Student("1004","Jane", 60, 80)
+        };
+        for (int i = 0; i < students.length; i++) {
+            students[i].print();
+        }
     }
 }
--- src/main/java/com/tom/Person.java
+++
@@ -1,4 +0,0 @@
-package com.tom;
-
-public class Person {
-}
