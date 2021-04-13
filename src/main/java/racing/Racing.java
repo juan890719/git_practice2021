db7a2485f059e5a87cf21c8ad7929fcaa82e5bd2
Juan <michelle.70623@gmail.com>
2021-04-13 16:14:23 +0800

    Thread and Interface exercise

---
+++ src/main/java/racing/Racing.java
@@ -0,0 +1,13 @@
+package racing;
+
+public class Racing {
+    public static void main(String[] args) {
+        Horse hr1 = new Horse();
+        Horse hr2 = new Horse();
+        HorseRunnable hr3 = new HorseRunnable();
+        Thread thread = new Thread(hr3);
+        hr1.start();
+        hr2.start();
+        thread.start();
+    }
+}
