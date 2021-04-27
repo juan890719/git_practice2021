0e1bae9b018c4d07b05e0fcf9ce904af61789a57
Juan <michelle.70623@gmaill.com>
2021-04-27 16:17:24 +0800

    array new object

--- src/main/java/test/Tester.java
+++ src/main/java/test/Tester.java
@@ -2,7 +2,7 @@ package test;
 
 public class Tester {
     public static void main(String[] args) {
-        Box[] boxes = {new Box1(), new Box2(), new Box3(), new Box4(), new Box5()};
+//        Box[] boxes = {new Box1(), new Box2(), new Box3(), new Box4(), new Box5()};
         int length = 10;
         int width = 5;
         int height= 5;
@@ -17,7 +17,15 @@ public class Tester {
 //        } else if (boxes[4].validate(length, width, height)) {
 //            System.out.println("Box5 it is!");
 //        }
+        Box[] boxes = new Box[5];
+        boxes[0] = new Box1();
+        boxes[1] = new Box2();
+        boxes[2] = new Box3();
+        boxes[3] = new Box4();
+        boxes[4] = new Box5();
+        System.out.println(boxes[3].price);
         for (Box box: boxes) {
+//            Box1 box1 = (Box1) box;
             if (box.validate(length, width, height)) {
                 System.out.println(box.name + " it is!, " + box.price);
                 break;
