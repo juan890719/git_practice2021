222f9650baa7da72369e7b94633dedd3c45fea63
juan <michelle.70623@gmail.com>
2021-06-01 14:58:48 +0800

    0601 add CarWin class

---
+++ src/main/java/com/tom/car/CarWin.java
@@ -0,0 +1,14 @@
+package com.tom.car;
+
+import javax.swing.*;
+
+public class CarWin {
+    //Variables are capitalized to represent constants(WIDTH)
+    static int WIDTH = 600;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH, 400);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
