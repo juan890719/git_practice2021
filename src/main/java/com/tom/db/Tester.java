2d80f1dc66fba07d911c24d3d064008cb1845757
juan <michelle.70623@gmail.com>
2021-06-22 16:37:53 +0800

    0615 continue mariadb

--- src/main/java/com/tom/db/Tester.java
+++ src/main/java/com/tom/db/Tester.java
@@ -1,9 +1,8 @@
 package com.tom.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tester {
     public static void main(String[] args) {
@@ -12,9 +11,26 @@ public class Tester {
             Class.forName("org.mariadb.jdbc.Driver");
             //Connection
             Connection connection = DriverManager
-                    .getConnection("jdbc:mariadb://localhost:3306/demo?user=root&password=123123555");
+                    .getConnection("jdbc:mariadb://localhost:3306/demo?user=root&password=123123555&useUnicode=true&characterEncoding=UTF-8");
             Statement statement = connection.createStatement();
-            statement.executeUpdate("insert into students values('000003', 'Jenny')");
+//            statement.executeUpdate("insert into students values('000004', 'Jay')");
+            ResultSet rs = statement.executeQuery("SELECT * FROM drinks");
+            List<Drink> drinks = new ArrayList<>();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                System.out.println(id + "/" + name + "/" + price);
+                Drink drink = new Drink(id, name, price);
+                drinks.add(drink);
+            }
+            //data collected
+            //print drinks information
+            System.out.println("Please insert coins for drinks:");
+            for (Drink drink: drinks) {
+//                System.out.println(drink);
+                System.out.println(drink.id + ") " + drink.name + "\t" + drink.price);
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException throwables) {
